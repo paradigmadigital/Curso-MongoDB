2b873bd92ef3c759c0e2bc142d7417b39b6734b7
Ernesto <evalero@paradigmatecnologico.com>
2015-11-17 11:32:33 +0100

    Añadido el script de creacion de entorno de shard

---
+++ Tema8/Sharding/startSharding.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+#Create path for config servers
+mkdir -p /data/configdb1
+mkdir -p /data/configdb2
+mkdir -p /data/configdb3
+
+#Start Config Server
+mongod --configsvr --dbpath /data/configdb1 --port 27019 &
+mongod --configsvr --dbpath /data/configdb2 --port 27029 & 
+mongod --configsvr --dbpath /data/configdb3 --port 27039 &
+
+#Start mongos
+mongos --configdb paradigma:27019,paradigma:27029,paradigma:27039 &
+
+#Create path for shard servers
+mkdir -p /data/shard1
+mkdir -p /data/shard2
+
+#Start shard servers
+mongod --shardsvr --replSet RS1 --dbpath /data/shard1 --port 27018 --noprealloc --nojournal &
+mongod --shardsvr --replSet RS2 --dbpath /data/shard2 --port 27028 --noprealloc --nojournal & 
+
+#initiate replica set for each shard
+mongo --port 27018 --shell rs.initiate(); exit;
+mongo --port 27028 --shell rs.initiate(); exit;
+
+#add shards to shard cluster
+
+
+
+
