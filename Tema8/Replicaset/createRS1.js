c7e53b7d8304f6f6565eee980b1977a51d68cf3a
Ernesto Valero <evalero@paradigmadigital.com>
2016-10-14 15:15:19 +0200

    Modifico el host de cada miembro del replica set

--- Tema8/Replicaset/createRS1.js
+++ Tema8/Replicaset/createRS1.js
@@ -1,12 +1,11 @@
 config = {
     _id : "RS1",
      members : [
-         {_id : 0, host : "paradigma:27017"},
-         {_id : 1, host : "paradigma:27018"},
-         {_id : 2, host : "paradigma:27019"}
+         {_id : 0, host : "mongodb:27017"},
+         {_id : 1, host : "mongodb:27018"},
+         {_id : 2, host : "mongodb:27019"}
      ]
 };
 db=db.getSiblingDB("admin");
 db.runCommand({replSetInitiate: config});
-exit(0);
-
+exit;
--- Tema8/Replicaset/startRS.sh
+++ Tema8/Replicaset/startRS.sh
@@ -5,9 +5,9 @@ mkdir -p /data/RS1_2
 mkdir -p /data/RS1_3
 
 #Start shard servers
-mongod --replSet RS1 --dbpath /data/RS1_1 --port 27017 --noprealloc --nojournal &
-mongod --replSet RS1 --dbpath /data/RS1_2 --port 27018 --noprealloc --nojournal &
-mongod --replSet RS1 --dbpath /data/RS1_3 --port 27019 --noprealloc --nojournal &
+mongod --replSet RS1 --dbpath /data/RS1_1 --fork --logpath /data/RS1_1/mongod.log --port 27017 --noprealloc --nojournal &
+mongod --replSet RS1 --dbpath /data/RS1_2 --fork --logpath /data/RS1_1/mongod.log --port 27018 --noprealloc --nojournal &
+mongod --replSet RS1 --dbpath /data/RS1_3 --fork --logpath /data/RS1_1/mongod.log --port 27019 --noprealloc --nojournal &
 
 #initiate replica set for each shard
 sleep 2
