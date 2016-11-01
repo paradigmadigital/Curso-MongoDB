18af979eee745c8ee198d4e5e46f9abe5c4e3552
Ernesto Valero <evalero@paradigmadigital.com>
2016-11-01 20:23:14 +0100

    Se cambia el nombre de replica set en los scripts createRS1.js y createRS2.js Se eliminan de ambos ficheros y de createConfig.js la instrucción exit

--- Tema8/Sharding/createRS1.js
+++ Tema8/Sharding/createRS1.js
@@ -1,9 +1,8 @@
 config = {
-    _id : "RS1",
+    _id : "shard1",
      members : [
          {_id : 0, host : "mongodb:27018"}
      ]
 };
 db=db.getSiblingDB("admin");
 db.runCommand({replSetInitiate: config});
-exit(0);
